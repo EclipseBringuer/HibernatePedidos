f513a2ff600b67417d3376af822d1f274a35ebe3
gabri <gabrielrl2004@gmail.com>
2023-11-20 19:06:08 +0100

    Implementación de Session

--- src/main/java/com/cesur/pedidoshibernate/Session.java
+++ src/main/java/com/cesur/pedidoshibernate/Session.java
@@ -1,4 +1,25 @@
 package com.cesur.pedidoshibernate;
 
+import com.cesur.pedidoshibernate.domain.entities.order.Order;
+import com.cesur.pedidoshibernate.domain.entities.user.User;
+
 public class Session {
+    private static User currentUser = null;
+    private static Order currentOrder = null;
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    public static void setCurrentUser(User currentUser) {
+        Session.currentUser = currentUser;
+    }
+
+    public static Order getCurrentOrder() {
+        return currentOrder;
+    }
+
+    public static void setCurrentOrder(Order currentOrder) {
+        Session.currentOrder = currentOrder;
+    }
 }
