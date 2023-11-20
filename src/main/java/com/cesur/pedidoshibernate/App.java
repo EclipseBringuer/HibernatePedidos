30a4450dcc350ce7d895d5e1d4db42d8c40e1876
gabri <gabrielrl2004@gmail.com>
2023-11-20 18:04:28 +0100

    Creación del DAO general

--- src/main/java/com/cesur/pedidoshibernate/App.java
+++ src/main/java/com/cesur/pedidoshibernate/App.java
@@ -1,5 +1,6 @@
 package com.cesur.pedidoshibernate;
 
+import com.cesur.pedidoshibernate.domain.HibernateUtil;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,6 +11,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        System.out.println(HibernateUtil.getSessionFactory());
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("fxml/login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
--- src/main/java/com/cesur/pedidoshibernate/domain/DAO.java
+++ src/main/java/com/cesur/pedidoshibernate/domain/DAO.java
@@ -1,4 +1,22 @@
 package com.cesur.pedidoshibernate.domain;
 
-public interface DAO {
+import java.util.ArrayList;
+
+/**
+ * DAO general para todas las clases
+ *
+ * @param <T> parametro que representa la entidad que queramos en la devolucion, Usuario,Juegos...
+ */
+public interface DAO<T> {
+
+    public ArrayList<T> getAll();
+
+    public T get(Long id);
+
+    public T save(T data);
+
+    public void update(T data);
+
+    public void delete(T data);
+
 }
