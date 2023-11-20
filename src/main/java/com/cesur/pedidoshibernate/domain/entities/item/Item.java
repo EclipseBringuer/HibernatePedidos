e4ea53d02bd14ed375a77ec0120941b6ad19ef03
gabri <gabrielrl2004@gmail.com>
2023-11-20 19:03:51 +0100

    Terminar de pulir las entidades y la relación

--- src/main/java/com/cesur/pedidoshibernate/domain/entities/item/Item.java
+++ src/main/java/com/cesur/pedidoshibernate/domain/entities/item/Item.java
@@ -1,6 +1,7 @@
 package com.cesur.pedidoshibernate.domain.entities.item;
 
 import com.cesur.pedidoshibernate.domain.entities.order.Order;
+import com.cesur.pedidoshibernate.domain.entities.product.Product;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -19,4 +20,49 @@ public class Item implements Serializable {
     @JoinColumn(name = "codigo_pedido", referencedColumnName = "codigo")
     private Order order;
 
+    @OneToOne
+    @JoinColumn(name = "id_producto")
+    private Product product;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", order=" + order.getCode() +
+                ", product=" + product +
+                '}';
+    }
 }
--- src/main/java/com/cesur/pedidoshibernate/domain/entities/product/Product.java
+++ src/main/java/com/cesur/pedidoshibernate/domain/entities/product/Product.java
@@ -1,4 +1,64 @@
 package com.cesur.pedidoshibernate.domain.entities.product;
 
-public class Product {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "producto")
+public class Product implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nombre")
+    private String name;
+
+    @Column(name = "precio")
+    private String price;
+
+    @Column(name = "cantidad")
+    private Integer amount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price='" + price + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
