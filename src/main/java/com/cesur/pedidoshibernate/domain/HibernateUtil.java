326bfb047385abaadf940a014f552573b1ea9f59
gabri <gabrielrl2004@gmail.com>
2023-11-20 18:00:33 +0100

    Implementación de HibernateUtil

--- src/main/java/com/cesur/pedidoshibernate/domain/HibernateUtil.java
+++ src/main/java/com/cesur/pedidoshibernate/domain/HibernateUtil.java
@@ -1,4 +1,25 @@
 package com.cesur.pedidoshibernate.domain;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
+
+    private static SessionFactory sf = null;
+
+    static {
+        try {
+            Configuration cfg = new Configuration();
+            cfg.configure();
+            sf = cfg.buildSessionFactory();
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sf;
+    }
 }
