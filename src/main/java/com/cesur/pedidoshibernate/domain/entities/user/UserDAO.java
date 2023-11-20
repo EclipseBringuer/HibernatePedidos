4017b983473cb6a913ed40422c58419093f02816
gabri <gabrielrl2004@gmail.com>
2023-11-20 19:32:12 +0100

    UserDAO implementado

--- src/main/java/com/cesur/pedidoshibernate/domain/entities/user/UserDAO.java
+++ src/main/java/com/cesur/pedidoshibernate/domain/entities/user/UserDAO.java
@@ -1,6 +1,9 @@
 package com.cesur.pedidoshibernate.domain.entities.user;
 
 import com.cesur.pedidoshibernate.domain.DAO;
+import com.cesur.pedidoshibernate.domain.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import java.util.ArrayList;
 
@@ -28,5 +31,23 @@ public class UserDAO implements DAO<User> {
     @Override
     public void delete(User data) {
 
+    }
+
+    public User validateUser(String email, String password) {
+        User result = null;
+
+        try (Session s = HibernateUtil.getSessionFactory().openSession()) {
+
+            Query<User> q = s.createQuery("FROM User u WHERE u.email=:em AND u.password=:pass", User.class);
+            q.setParameter("em", email);
+            q.setParameter("pass", password);
+
+            try{
+                result = q.getSingleResult();
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+        return result;
     }
 }
